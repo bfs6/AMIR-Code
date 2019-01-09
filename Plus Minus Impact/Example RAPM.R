28abecc9f33c4f4eb917edfe8e52227a686b31d4
Basil Seif <basil.seif@duke.edu>
2019-01-09 14:54:12 -0500

    Update Example RAPM.R
    RAPM Models for 2016 and 2017

--- Plus Minus Impact/Example RAPM.R
+++ Plus Minus Impact/Example RAPM.R
@@ -1,116 +1,107 @@
-####Read in Libraries####
-library(RMySQL)
-library(DBI)
+####Load Libraries####
+library(ggplot2)
+library(broom)
+library(MASS)
+library(dplyr)
 
+####Load and Run RAPM Counting Function####
+##Load
+source(paste0(getwd(), "/RAPM Counting Function v2.R"))
 
-
-####Read in Data####
-##Read in all Games from 2016 Season
-mydb_pbp <- dbConnect(RMySQL::MySQL(), user = "root", password = "B@zm@x123!", dbname = "nba_pbp_data_scrape", host = "35.199.55.188")
-all_games_list <- dbListTables(mydb_pbp)
-all_games_2k16 <- all_games_list[which(grepl("play_by_play_game_id_00216", all_games_list) == T)]
-game_list <- list()
-for(i in seq_along(all_games_2k16)){
-  print(i)
-  game_list[[i]] <- dbReadTable(mydb_pbp, all_games_2k16[i])
+##Run Function from 1998 to 2018
+stint_list <- list()
+for(i in c(1998:2018)){
+  stint_list[[i]] <- rapm_counting_function(year = i)
 }
-dbDisconnect(mydb_pbp)
+names(stint_list) <- c(1998:2018)
 
 
-##List of Players from 2016 Season
+##Practice w/ 2016
+stint2k16 <- rapm_counting_function(year = 2016)
+stint2k17 <- rapm_counting_function(year = 2017)
+
+####Run RAPM 2016####
+##Get Rid of Low Minutes 
+year = 2016
 mydb_bbref <- dbConnect(RMySQL::MySQL(), user = "root", password = "B@zm@x123!", dbname = "basketball_ref_scrape", host = "35.199.55.188")
 per_game_stats <- dbReadTable(mydb_bbref, "per_game_stats")
-player_df <- data.frame(player = per_game_stats$player[which(per_game_stats$season == 2017)],
-                        id = per_game_stats$link[which(per_game_stats$season == 2017)])
+player_df <- data.frame(player = per_game_stats$player[which(per_game_stats$season == year + 1)],
+                        id = per_game_stats$link[which(per_game_stats$season == year + 1)],
+                        games_played = per_game_stats$g[which(per_game_stats$season == year + 1)],
+                        minutes_per_game = per_game_stats$mp[which(per_game_stats$season == year + 1)])
+player_df$minutes_played <- player_df$games_played * player_df$minutes_per_game
+player_df <- subset(player_df, select = -c(games_played, minutes_per_game))
+player_df <- player_df %>% group_by(player, id) %>% dplyr::summarize(minutes_played = sum(minutes_played))
 rm(per_game_stats)
 dbDisconnect(mydb_bbref)
 
+low_min_players <- player_df[which(player_df$minutes_played <= 250), ]
+low_min_players2 <- player_df[which(player_df$minutes_played <= 1000), ]
 
-####Initialize Stint Matrix and Response Vectors####
-stints <- as.data.frame(matrix(NA,0,length(player_df$player)))
-names(stints) <- player_df$player
-stintCount = 1
+lowstints <- stint2k16[, -which(names(stint2k16) %in% low_min_players$player)]
+lowstints3 <- lowstints[which(rowSums(abs(lowstints[,1:(length(lowstints)-1)])) == 10), ]
 
-##Partition Actions that Terminate Possessions
-game <- game_list[[1]]
-game$home_score <- sapply(seq_along(game$SCORE), function(x) as.numeric(strsplit(game$SCORE[x], " - ")[[1]][2]))
-game$away_score <- sapply(seq_along(game$SCORE), function(x) as.numeric(strsplit(game$SCORE[x], " - ")[[1]][1]))
-off_rebound <- sapply(seq_along(game$GAME_ID)[-1],
-                      function(x){
-                        if(game$EVENTMSGTYPE[x] == 4 & game$EVENTMSGTYPE[x-1] == 2 & is.na(game$HOMEDESCRIPTION[x]) == T & is.na(game$HOMEDESCRIPTION[x-1]) == T){
-                          1
-                        }else if(game$EVENTMSGTYPE[x] == 4 & game$EVENTMSGTYPE[x-1] == 2 & is.na(game$HOMEDESCRIPTION[x]) == F & is.na(game$HOMEDESCRIPTION[x-1]) == F){
-                          1
-                        }else{
-                          0
-                        }
-                      })
-game$off_rebound <- c(0, off_rebound)
-game$def_rebound <- rep(0, nrow(game))
-game$def_rebound[which(game$EVENTMSGTYPE == 4 & game$off_rebound == 0)] = 1
-shots <- game[sort(c(which(game$EVENTMSGTYPE %in% c(1:3, 5)), which(game$EVENTMSGTYPE == 4 & game$def_rebound == 1))),]
-
-##Home and Away Starting Lineups
-awayplayersStart <- sort(unique(unlist(shots[1,paste0("Away_Player_", c(1:5))])))
-homeplayersStart <- sort(unique(unlist(shots[1,paste0("Home_Player_", c(1:5))])))
-
-##Walk Through Shot Situations and Check when Lineups Change
-for(i in seq_along(shots$GAME_ID)){
-  awayplayers <- sort(unique(unlist(shots[i,paste0("Away_Player_", c(1:5))])))
-  homeplayers <- sort(unique(unlist(shots[i,paste0("Home_Player_", c(1:5))])))
-  bothHome <- homeplayersStart %in% homeplayers
-  bothAway <- awayplayersStart %in% awayplayers
-  if(all(bothHome) == T & all(bothAway) == T){
-    if(shots[i, c("EVENTMSGTYPE")] == 1){
-      possessions = possessions + 1
-      
-      
-      ######GOTTA ADD POINTS OF STINTS######
-      
-      
-      
-      
-    }else if(shots[i, c("EVENTMSGTYPE")] == 2){
-      possessions = possessions + 1
-    }else if(shots[i, c("def_rebound")] == 1){
-      possessions = possessions + 1
-    }else if(shots[i, c("EVENTMSGTYPE")] == 5){
-      possessions = possessions + 1
-    }else if(shots[i, c("EVENTMSGTYPE")] == 3){
-      # midFreeThrow = shots[i, ""]
-      if(shots[i, c("EVENTMSGACTIONTYPE")] == 10){
-        totalToShoot = 1
-        currentShoot = 1
-      }else if(shots[i, c("EVENTMSGACTIONTYPE")] == 11){
-        totalToShoot = 1
-        currentShoot = 2
-      }else if(shots[i, c("EVENTMSGACTIONTYPE")] == 12){
-        totalToShoot = 2
-        currentShoot = 2
-      }else if(shots[i, c("EVENTMSGACTIONTYPE")] == 13){
-        totalToShoot = 1
-        currentShoot = 3
-      }else if(shots[i, c("EVENTMSGACTIONTYPE")] == 14){
-        totalToShoot = 2
-        currentShoot = 3
-      }else if(shots[i, c("EVENTMSGACTIONTYPE")] == 15){
-        totalToShoot = 3
-        currentShoot = 3
-      }else if(shots[i, c("EVENTMSGACTIONTYPE")] == 16){
-        totalToShoot = 1
-        currentShoot = 1
-      }
-      if(totalToShoot > currentShoot){
-        if(awayScore == )
-      }
-    }
-  }
-}
+lowstints2 <- stint2k16[, -which(names(stint2k16) %in% low_min_players2$player)]
+lowstints4 <- lowstints2[which(rowSums(abs(lowstints2[,1:(length(lowstints2)-1)])) == 10), ]
 
 
+##Run APM Model
+apm <- lm(formula = Margin~., data = lowstints4)
+summary(apm)
+sort(apm$coefficients, decreasing = T)
 
 
+##Run RAPM Model
+stint2k16_clean <- stint2k16[,which(colSums(abs(stint2k16)) != 0)]
+rapm2 <- lm.ridge(formula = Margin~., data = stint2k16_clean, lambda = seq(0,80000,400))
+r2 <- tidy(rapm2)
+g <- glance(rapm2)
+ggplot(r2, aes(lambda, GCV)) + geom_line() + geom_vline(xintercept = g$lambdaGCV, col = "red", lty = 2)
+
+results <- sort(rapm2$coef[,which(as.numeric(colnames(rapm2$coef)) == g$lambdaGCV)], decreasing = T)
+names(results) <- gsub("`", "", names(results))
+highminresults <- results[-which(names(results) %in% low_min_players2$player)]
 
 
+####Run RAPM 2017####
+##Get Rid of Low Minutes 
+year = 2017
+mydb_bbref <- dbConnect(RMySQL::MySQL(), user = "root", password = "B@zm@x123!", dbname = "basketball_ref_scrape", host = "35.199.55.188")
+per_game_stats <- dbReadTable(mydb_bbref, "per_game_stats")
+player_df <- data.frame(player = per_game_stats$player[which(per_game_stats$season == year + 1)],
+                        id = per_game_stats$link[which(per_game_stats$season == year + 1)],
+                        games_played = per_game_stats$g[which(per_game_stats$season == year + 1)],
+                        minutes_per_game = per_game_stats$mp[which(per_game_stats$season == year + 1)])
+player_df$minutes_played <- player_df$games_played * player_df$minutes_per_game
+player_df <- subset(player_df, select = -c(games_played, minutes_per_game))
+player_df <- player_df %>% group_by(player, id) %>% dplyr::summarize(minutes_played = sum(minutes_played))
+rm(per_game_stats)
+dbDisconnect(mydb_bbref)
 
+low_min_players <- player_df[which(player_df$minutes_played <= 125), ]
+low_min_players2 <- player_df[which(player_df$minutes_played <= 250), ]
+
+lowstints <- stint2k17[, -which(names(stint2k17) %in% low_min_players$player)]
+lowstints3 <- lowstints[which(rowSums(abs(lowstints[,1:(length(lowstints)-1)])) == 10), ]
+
+lowstints2 <- stint2k17[, -which(names(stint2k17) %in% low_min_players2$player)]
+lowstints4 <- lowstints2[which(rowSums(abs(lowstints2[,1:(length(lowstints2)-1)])) == 10), ]
+
+
+##Run APM Model
+apm <- lm(formula = Margin~., data = lowstints4)
+summary(apm)
+sort(apm$coefficients, decreasing = T)
+
+
+##Run RAPM Model
+stint2k17_clean <- stint2k17[,which(colSums(abs(stint2k17)) != 0)]
+rapm2_2017 <- lm.ridge(formula = Margin~., data = stint2k17_clean, lambda = seq(0,80000,400))
+r2 <- tidy(rapm2_2017)
+g <- glance(rapm2)
+ggplot(r2, aes(lambda, GCV)) + geom_line() + geom_vline(xintercept = g$lambdaGCV, col = "red", lty = 2)
+
+results <- sort(rapm2_2017$coef[,which(as.numeric(colnames(rapm2_2017$coef)) == g$lambdaGCV)], decreasing = T)
+names(results) <- gsub("`", "", names(results))
+highminresults <- results[-which(names(results) %in% low_min_players2$player)]
 
